c8b82441d7f87f161dc210bfe27ffc27edcbdcb7
Daniel Silva <danielzcomputacao@gmail.com>
2023-09-04 20:08:35 -0300

    Ex01 done

---
+++ ConditionalExercises/Ex01.c
@@ -0,0 +1,23 @@
+// Write a program that reads two numbers and prints the larger one.
+
+#include <stdio.h>
+
+int main()
+{
+    float n1, n2, bigger;
+    printf("Type the first number: ");
+    scanf("%f", &n1);
+    printf("Type the second number: ");
+    scanf("%f", &n2);
+
+    if (n1 > n2)
+    {
+        bigger = n1;
+    }
+    else
+    {
+        bigger = n2;
+    }
+
+    printf("The larger number is %.2f", bigger);
+}
