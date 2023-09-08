71b41f99b6ab9f135ee8c68024ca5e0fcb2418dc
Daniel Silva <danielzcomputacao@gmail.com>
2023-09-08 17:50:23 -0300

    Matrix

---
+++ Matrix/Matrix.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int nLin = 3;
+    int nCol = 3;
+    int mat[nLin][nCol];
+
+    for (int i = 0; i < nLin; i++)
+    {
+        for (int j = 0; j < nCol; j++)
+        {
+            printf("Digite o valor da linha %d coluna %d: ", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+    for (int i = 0; i < nLin; i++)
+    {
+        for (int j = 0; j < nCol; j++)
+        {
+            printf("%d", mat[i][j]);
+            printf(" ");
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < nLin; i++)
+    {
+        for (int j = 0; j < nCol; j++)
+        {
+            if (i == j)
+            {
+                printf("%d", mat[i][j]);
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < nLin; i++)
+    {
+        for (int j = 0; j < nCol; j++)
+        {
+            if (i + j == 2)
+            {
+                printf("%d", mat[i][j]);
+                printf(" ");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < nLin; i++)
+    {
+        for (int j = 0; j < nCol; j++)
+        {
+            if (i > j)
+            {
+                printf("%d", mat[i][j]);
+                printf(" ");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
