cc74a36b03d09cae999980695d7ed487e8432981
Daniel Silva <danielzcomputacao@gmail.com>
2023-09-09 15:31:25 -0300

    Ex02 done

---
+++ Matrix/Ex02.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nRow = 4;
+    int nCol = 4;
+    int mat[nRow][nCol];
+
+    for (int i = 0; i < nRow; i++)
+    {
+        for (int j = 0; j < nCol; j++)
+        {
+            printf("Digite o valor da linha %d coluna %d: ", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    for (int i = 0; i < nRow; i++)
+    {
+        for (int j = 0; j < nCol; j++)
+        {
+            if ((mat[i][j] > 15) && (mat[i][j] < 25))
+            {
+                printf("%d", mat[i][j]);
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
